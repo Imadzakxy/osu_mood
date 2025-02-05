51eb58c214644e70736acf3aac0302027f3d2848
Imadzakxy <imadzakxy@gmail.com>
2025-02-05 20:36:39 +0100

    50% fini

--- script.js
+++ script.js
@@ -2,50 +2,76 @@ let content = document.querySelector('.content');
 let bot = document.querySelector('.bot');
 let pts = document.querySelector('.pts');
 let game = document.querySelector('.game');
-const fileUpload = document.getElementById('file-upload');
-const audioPreview = document.getElementById('audio-preview');
-let audio = document.getElementById('background-music');
+let audio = document.getElementById('music');
 let game_over = false;
 
+function check(){
+    if(window.matchMedia("(orientation: portrait)").matches) {
+        document.getElementById("warning").style.display = "block";
+    } else {
+        document.getElementById("warning").style.display = "none";
+    }
+}
 
-bot.onclick = function() {
-    startGame();
-};
+window.addEventListener("load",check);
+window.addEventListener("resize",check);
 
 document.getElementById('image-upload').addEventListener('change', function(event) {
-    let file = localStorage.getItem('image');
-    file = event.target.files[0];
+    let file = event.target.files[0];
     const preview = document.getElementById('preview');
     
-    if (file && file.type.startsWith('image/')) { 
-
+    if (file && file.type.startsWith('image/')) {
         const reader = new FileReader();
         reader.onload = function(e) {
             preview.src = e.target.result;
             preview.style.display = 'block';
             game.style.backgroundImage = `url(${e.target.result})`;
-            localStorage.setItem('image', file);
+            localStorage.setItem('image', preview.src);
         };
         reader.readAsDataURL(file);
     }
 });
 
 document.getElementById('file-upload').addEventListener('change', function(event) {
-    let file = localStorage.getItem('audio');
-    file = event.target.files[0];
-    
-    if(file && file.type.startsWith('audio/')) {
+    let file = event.target.files[0];
 
-        const audio = document.getElementById('background-music');
-        
-        if(audio.src.startsWith('blob:')) {
-            URL.revokeObjectURL(audio.src);
-        }
-
-        audio.src = URL.createObjectURL(file);
-        localStorage.setItem('audio', audio); 
+    if (file && file.type.startsWith('audio/')) { 
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            audio.src = URL.createObjectURL(file);
+            localStorage.setItem('audio', audio.src); 
+        };
+        reader.readAsDataURL(file);
     }
+    
 });
+
+function loadimage() {
+    let image = localStorage.getItem('image');
+    
+    if (image) {
+        preview.src = image;
+        game.style.backgroundImage = `url(${image})`;
+    } 
+}
+
+function loadaudio() {
+    let music = localStorage.getItem('audio');
+
+    if (music) {
+        audio.src = music;
+        audio.load();
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadimage();
+    loadaudio();
+});
+
+bot.onclick = function() {
+    startGame();
+};
 
 function startGame() {
     if(!game_over){
